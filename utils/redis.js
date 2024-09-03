4965941fe74487d91647bf66d1a38a79d240ecb0
Mainasploit <140205210+Mainasploit@users.noreply.github.com>
2024-09-03 14:21:58 +0300

    Update redis.js

--- utils/redis.js
+++ utils/redis.js
@@ -4,33 +4,55 @@ const { createClient } = require('redis');
 const { promisify } = require('util');
 
 class RedisClient {
-  constructor () {
+  constructor() {
     this.client = createClient();
-    this.client.on('error', (err) => console.log(err));
-    this.connected = false;
+    this.client.on('error', (err) => {
+      console.error('Redis error:', err.message);
+      this.connected = false;
+    });
     this.client.on('connect', () => {
+      console.log('Redis connected');
       this.connected = true;
+    });
+    this.client.on('end', () => {
+      console.log('Redis disconnected');
+      this.connected = false;
     });
   }
 
-  isAlive () {
+  isAlive() {
     return this.connected;
   }
 
-  async get (key) {
-    const getAsync = promisify(this.client.get).bind(this.client);
-    const val = await getAsync(key);
-    return val;
+  async get(key) {
+    try {
+      const getAsync = promisify(this.client.get).bind(this.client);
+      const value = await getAsync(key);
+      return value;
+    } catch (err) {
+      console.error('Redis GET error:', err.message);
+      throw err;
+    }
   }
 
-  async set (key, val, dur) {
-    const setAsync = promisify(this.client.set).bind(this.client);
-    await setAsync(key, val, 'EX', dur);
+  async set(key, value, duration) {
+    try {
+      const setAsync = promisify(this.client.set).bind(this.client);
+      await setAsync(key, value, 'EX', duration);
+    } catch (err) {
+      console.error('Redis SET error:', err.message);
+      throw err;
+    }
   }
 
-  async del (key) {
-    const delAsync = promisify(this.client.del).bind(this.client);
-    await delAsync(key);
+  async del(key) {
+    try {
+      const delAsync = promisify(this.client.del).bind(this.client);
+      await delAsync(key);
+    } catch (err) {
+      console.error('Redis DEL error:', err.message);
+      throw err;
+    }
   }
 }
 
